9597c7de4d94069742ea76cfa8583a576e8c3be0
tuananhphan97vn <112112896+tuananhphan97vn@users.noreply.github.com>
2023-07-20 14:06:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python run_squad_roberta.py \
 	--model_type roberta \
-	--output_dir nblock1_28_2_2023_s1 \
+	--output_dir output_dir \
 	--evaluate_during_training \
 	--do_lower_case \
 	--learning_rate 1e-5 \
@@ -16,21 +16,14 @@ python run_squad_roberta.py \
 	--save_steps 2000 \
 	--seed 1 \
 	--threads 16 \
-	--train_file  /storage-nlp/public-dataset/newsqa/train.json \
+	--train_file  newsqa/train.json \
 	--model_name_or_path roberta-base \
 	--do_train\
 	--overwrite_output_dir\
 	--cache_dir ./cache \
-	--predict_file  /storage-nlp/public-dataset/newsqa/dev.json \
+	--predict_file  newsqa/dev.json \
 	--n_block 0 \
 	--n_block_reasoning 1 --warmup_steps 1000 \
 	--word_dim 768 \
 	--sent_dim 768 --chunk_length 0 --version_2_with_negative
-	#--tokenizer_name ~/work/huggingface/vinai/phobert-base \
-	# --config_name ~/work/huggingface/vinai/phobert-base \
-	# --eval_all_checkpoint \
-	#--version_2_with_negative
-	# --null_score_diff_threshold 0.0
-	#--predict_file /data/nlp/hoangnv74/question-answering/data/viquad/test_positive.json \
-	# --do_eval \
 
